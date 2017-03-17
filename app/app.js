6fc65cda1112bdc1b13fcad8bcee8d5e78298398
Jonathas Amaral <jnthas@gmail.com>
2017-03-16 22:15:00 -0300

    project angular-seed adapted

--- app/app.js
+++ app/app.js
@@ -2,13 +2,23 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+])
+.config(['$urlRouterProvider', function ($urlRouterProvider) {	
 
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $urlRouterProvider.otherwise('/view1');
+
+}])
+
+.config(['$stateProvider', function ($stateProvider) {
+
+	$stateProvider.state({
+		name: 'app',
+		template: '<ui-view></ui-view>',
+		abstract: true
+	});
+
 }]);
